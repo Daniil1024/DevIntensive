edc33a41de3f890c49a6194e4bef1038c0063a87
Daniil <den14072004@mail.ru>
2016-07-07 20:14:24 +0600

    homatask 4

--- app/src/main/java/com/softdesign/devintensive/data/managers/PrefencesManager.java
+++ app/src/main/java/com/softdesign/devintensive/data/managers/PrefencesManager.java
@@ -1,6 +1,7 @@
 package com.softdesign.devintensive.data.managers;
 
 import android.content.SharedPreferences;
+import android.net.Uri;
 
 import com.softdesign.devintensive.utils.ConstantManager;
 import com.softdesign.devintensive.utils.DevintensiveApplication;
@@ -41,4 +42,13 @@ public class PrefencesManager {
         return userFields;
     }
 
+    public void saveUserPhoto(Uri uri) {
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+        editor.putString(ConstantManager.USER_PHOTO_KEY, uri.toString());
+        editor.apply();
+    }
+
+    public Uri loadUserPhoto() {
+        return Uri.parse(mSharedPreferences.getString(ConstantManager.USER_PHOTO_KEY, "android.resource://com.sofdesign.devintensive/drawable/userphoto"));
+    }
 }
